4a6479f157aa2a4768ea4d4974046a999d4b4572
Noah Gorstein <ngorstein@gmail.com>
2017-11-12 22:26:01 -0500

    Add form for messages and display incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,29 @@ socket.on('disconnect', function(){
 	console.log('disconnected from server!');
 });
 
-//listener for newMessager from server
+//listener for newMessage from server
 socket.on('newMessage', function (message){
 	console.log('newMessage', message);
+	var li = $('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	$('#messages').append(li);
 });
+
+socket.emit('createMessage', {
+	from: 'Noah',
+	text: 'new message from noah'
+	}, function (data) {
+		console.log('Acknowledged on client side', data);
+	});
+
+$('#message-form').on('submit', function(e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: $('[name=message]').val()
+	}, function(){
+
+	});
+})
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -16,33 +17,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log("new user connected");
 
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'Welcome to Chalk Talk',
-		createdAt: new Date().getTime()
-	});
-
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'New user joined!',
-		createdAt: new Date().getTime()
-	});
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to chalk talk'));
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 	//listening for client's 'createMessage'
-	socket.on('createMessage', (message) => {
-		// console.log('createMessage', message);
+	socket.on('createMessage', (message, callback) => {
+		console.log('createMessage', message);
 		//io emits to every person
-		io.emit('newMessage', {
-			from: message.from,
-			text:message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage', generateMessage(message.from, message.text));
+		callback('This is from the server!');
 		// broadcast sends to everyone but MYSELF
-		socket.broadcast.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', (socket) => {
