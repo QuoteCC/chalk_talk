2bcea7dccaf012c84970db6799f607c229e1aef5
tarinrickett <trickett96@gmail.com>
2017-12-02 15:48:50 -0500

    fix jQuery run

--- public/js/tarin.js
+++ public/js/tarin.js
@@ -1,12 +1,15 @@
-
-// icon hide/display
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  let newIcon = e.target.children[0];
-  let oldIcon = e.relatedTarget.children[0];
-  if (newIcon.classList.contains('fa-window-close')) {
-    newIcon.classList.remove('invisible');
-  }
-  if (oldIcon.classList.contains('fa-window-close')) { 
-    oldIcon.classList.add('invisible');
-  }
-})
+$(document).ready(function() {
+  // icon hide/display
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    let newIcon = e.target.children[0];
+    let oldIcon = e.relatedTarget.children[0];
+    console.log('new: ', newIcon);
+    console.log('old: ', oldIcon);
+    if (newIcon.classList.contains('fa-window-close')) {
+      newIcon.classList.remove('invisible');
+    }
+    if (oldIcon.classList.contains('fa-window-close')) {
+      oldIcon.classList.add('invisible');
+    }
+  })
+});
